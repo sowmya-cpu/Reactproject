a81d7ef654b688a5b0bc53cb854ee7658b0a052c
sowmya-cpu <gvvsowmya16@gmail.com>
2025-03-22 02:44:11 +0530

    Fixed JSX errors and removed unused imports

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react"
 import { Menu, X } from "lucide-react"
 import Link from "next/link"
-import Image from "next/image"
+
 
 const navItems = [
   { name: "Home", href: "/" },
